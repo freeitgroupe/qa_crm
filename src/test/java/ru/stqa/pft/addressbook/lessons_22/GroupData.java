8dde346b4bf01c09b59aef7892eaaa0afbfddde1
freeitgroupe <freeitgroupe@gmail.com>
2020-11-24 15:41:33 +0200

    lessons_22

---
+++ src/test/java/ru/stqa/pft/addressbook/lessons_22/GroupData.java
@@ -0,0 +1,28 @@
+package ru.stqa.pft.addressbook.lessons_22;
+
+public class GroupData {
+  private final String name;
+  private final String header;
+  private final String footer;
+
+  //конструктор для инициализации параметров, которые необходимо передать в объект класса
+  public GroupData(String name, String header, String footer) {
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+  //
+  public String getName() {
+    return name;
+  }
+
+  //
+  public String getHeader() {
+    return header;
+  }
+
+  //
+  public String getFooter() {
+    return footer;
+  }
+}
