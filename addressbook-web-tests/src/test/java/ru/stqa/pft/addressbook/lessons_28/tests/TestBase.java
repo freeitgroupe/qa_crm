b4b2b8fbb37b9778aa9755faec03704eb15d66a0
freeitgroupe <freeitgroupe@gmail.com>
2020-12-02 16:03:11 +0200

    lessons_30

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/lessons_28/tests/TestBase.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.lessons_28.tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import ru.stqa.pft.addressbook.lessons_28.appmanager.ApplicationManager;
+
+public class TestBase {
+  //По факту создаем объект вспомогательного класса
+  protected final ApplicationManager app = new ApplicationManager();
+
+  @BeforeClass
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterMethod
+  public void teardown(){
+    app.stop();
+  }
+
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/lessons_30/appmanager/GroupHelper.java
@@ -0,0 +1,52 @@
+package ru.stqa.pft.addressbook.lessons_30.appmanager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.stqa.pft.addressbook.lessons_30.model.GroupData;
+
+public class GroupHelper extends HelperBase {
+
+  public GroupHelper(WebDriver driver) {
+    super(driver);
+  }
+
+  /*Возврат к списку группы*/
+  public void returnToGroupPage() {
+    click(By.xpath("//div[@id='content']/div"));
+    click(By.linkText("group page"));
+  }
+  /*Клик по кнопке создания группы*/
+  public void submitGroupCreation() {
+    click(By.name("submit"));
+  }
+
+  /*Заполнение полей формы*/
+  public void fillGroupForm(GroupData groupData) {
+    type(By.name("group_name"), groupData.getName());
+    type(By.name("group_header"), groupData.getHeader());
+    type(By.name("group_footer"), groupData.getFooter());
+  }
+
+  /*Создание группы а именно Клик по кнопке создания группы*/
+  public void initGroupCreation() {
+    click(By.name("new"));
+  }
+
+  /*Удаление группы а именно клик по кнопке удаления группы*/
+  public void deleteSelectedGroups() {
+    click(By.name("delete"));
+  }
+
+  /*Выбор группы, а именно клик по указанному checkbox */
+  public void selectGroup() {
+    click(By.name("selected[]"));
+  }
+
+  /*Клик по кнопке модификации группы*/
+  public void initGroupModification(){
+    click(By.name("edit"));
+  }
+  /*Клик по кнопке для сохранения результата*/
+  public void submitGroupModification() {
+    click(By.name("update"));
+  }
+}
