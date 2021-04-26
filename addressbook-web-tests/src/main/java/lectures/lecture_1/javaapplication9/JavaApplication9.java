ab8a51eaf9c06e20ed6e5bdb0fd4424254d0e234
freeitgroupe <freeitgroupe@gmail.com>
2021-04-26 16:28:59 +0300

    lecture.1.3

--- addressbook-web-tests/src/main/java/lectures/lecture_1/javaapplication9/JavaApplication9.java
+++ addressbook-web-tests/src/main/java/lectures/lecture_1/javaapplication9/JavaApplication9.java
@@ -3,8 +3,11 @@ package lectures.lecture_1.javaapplication9;
 public class JavaApplication9 {
     public static void main(String[] args) {
         Point2D p1 = new Point2D();
-        p1.setX(-1);
-        p1.setY(2);
-        System.out.println(p1);
+        p1.setX(1);
+        p1.setY(1);
+        System.out.println(p1 + ", length=" + p1.getLength());
+
+        Point2D p2 = new Point2D(3,4);
+        System.out.println(p2 + ", length=" + p2.getLength());
     }
 }
--- addressbook-web-tests/src/main/java/lectures/lecture_1/javaapplication9/Point2D.java
+++ addressbook-web-tests/src/main/java/lectures/lecture_1/javaapplication9/Point2D.java
@@ -2,24 +2,46 @@ package lectures.lecture_1.javaapplication9;
 
 public class Point2D {
     private int x, y;
+    private double length; // длина вектора.
+
+    public Point2D(){
+        // Так как на момент создания конструктора объект размещен в памяти тогда мы можем воспользовться ссылкой this.
+        // обращаемся к другому конструктору нашего класса
+        this(0,0);
+    }
+
+    /*public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }*/
+
+    // Чтобы нельзя было переопределить процесс создания объектов, сеттеры объявляем как final
+    // Методы вызываемые из конструктора должны быть final, чтобы не сломать процесс создания объектов
+    //
+    public Point2D(int x, int y) {
+        setX(x);
+        setY(y);
+    }
 
     public int getX() {
         return x;
     }
 
-    public void setX(int x) {
+    public final void setX(int x) {
         this.x = x;
+        length();
     }
 
     public int getY() {
         return y;
     }
 
-    public void setY(int y) {
+    public final void setY(int y) {
         this.y = y;
+        length();
     }
 
-    @Override //аннотция обозначающая переопределение метода
+    @Override //аннотация обозначающая переопределение метода
     public String toString() {
         StringBuilder sb = new StringBuilder("(");
         sb.append(x);
@@ -28,5 +50,14 @@ public class Point2D {
         sb.append(")");
         return sb.toString();
         //return "Point2D{" + "x=" + x + ", y=" + y + '}';// так писать нельзя это увелич нагрузку на gc, надо использовать StringBuilder
+    }
+
+    // Длина радиус вектора
+    private void length(){
+        length = Math.sqrt(x*x + y*y);
+    }
+
+    public double getLength() {
+        return length;
     }
 }
